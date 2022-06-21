f841d8cdd705ca6d4c438d062563b06a0d08fd26
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 16:11:56 -0300

    feat: create LoginView using DRF authtoken

--- users/views.py
+++ users/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView, Response, status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
 
 class UserView(APIView):  
-
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)   
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(
+            username = serializer.validated_data['email'],
+            password = serializer.validated_data['password']
+        )
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key})
+        return Response(
+            {"detail": "Invalid email or password"}, status.HTTP_401_UNAUTHORIZED
+        )
