54cd1182f4f9717df02ea9f32e6d7eb0a29bdd6b
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 16:04:07 -0300

    feat: create MovieView and MovieViewDetail

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView, Response, status
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieView(APIView):
+    def get(self, _):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+class MovieViewDetail(APIView):
+    def get(self, resquest, movie_id):
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response({"message": "Movie not found"}, status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
