5c60bbff997549e76ec237eed9954e0b4160b17d
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-20 21:35:06 -0300

    feat: create User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .utils import CustomUserManager
+from datetime import datetime
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(unique=False, null=True, max_length=255)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    updated_at = models.DateTimeField(default=datetime.now)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = CustomUserManager()
