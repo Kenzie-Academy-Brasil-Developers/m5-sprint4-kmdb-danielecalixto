b2dca998309ac55d892e9ed1731e5f4bcdaa1743
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-20 22:11:52 -0300

    feat: create UserSerializer

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from users.models import User
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all(), message='This email already exists.')])
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    date_joined = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        return user
