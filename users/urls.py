7c3f3f4d892efe3352dc744d9f5ecf162523cc52
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 16:11:11 -0300

    feat: create users urls using DRF authtoken

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
+#using Django Token Authentication without JWT
+from rest_framework.authtoken import views
 
-from . import views
+from .views import UserView, LoginView
+
 
 urlpatterns = [
-    path("users/register/", views.UserView.as_view()),
+    path("users/register/", UserView.as_view()),
+    path("token-auth", views.obtain_auth_token),
+    path("users/login/", LoginView.as_view()),
 ]
+
+
+# To use jwt:
+
+# from rest_framework_simplejwt import views
+
+# path("users/login/", views.TokenObtainPairView.as_view()),
+# path("token/refresh/", views.TokenRefreshView.as_view()),
