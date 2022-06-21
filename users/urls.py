88a3edee0a483236807a89f728421af093b0e9a7
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 16:57:50 -0300

    fix: create users/ path to use UseView and include RegisterView to user/register/ path

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 #using Django Token Authentication without JWT
 from rest_framework.authtoken import views
 
-from .views import UserView, LoginView, UserViewDetail
+from .views import RegisterView, UserView, LoginView, UserViewDetail
 
 urlpatterns = [
-    path("users/register/", UserView.as_view()),
+    path("users/register/", RegisterView.as_view()),
+    path("users/", UserView.as_view()),
     path("users/<int:user_id>/", UserViewDetail.as_view()),
     path("token-auth", views.obtain_auth_token),
     path("users/login/", LoginView.as_view()),
