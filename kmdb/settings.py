b81df340658de63fa117ce874213fb84950612d1
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 16:09:41 -0300

    fix: add settings to use simplejwt auth in INSTALLED_APPS and REST_FRAMEWORK / add settings to use DRF authtoken in INSTALLED_APPS

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'users',
     'movies',
     'genres',
@@ -128,3 +130,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
