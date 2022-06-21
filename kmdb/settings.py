4c1a3396837e853cdd77ef509e96d4985f4b7290
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 18:25:11 -0300

    fix: comment DEFAULT_ATHENTICATION_CLASSES of REST_FRAMEWORK project settings.py for not globally required authentication

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ],
+# }
