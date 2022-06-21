62dd8069a2c8f4d359ba64898baaf5c3400dda6e
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 20:51:41 -0300

    fix: change recommendation to choice field

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+class RecommendationType(models.TextChoices):
+    MUST = ("M", "Must Watch")
+    SHOULD = ("S", "Should Watch")
+    AVOID = ("A", "Avoid Watch")
+    NO = ("NO", "No Opinion")
+
 class Review(models.Model):
     stars = models.IntegerField()
     review = models.TextField()
     spoilers = models.BooleanField()
-    recommendation = models.CharField(max_length=50)
+    recommendation = models.CharField(
+        max_length=50,
+        choices=RecommendationType.choices,
+        default=RecommendationType.NO
+    )
 
     movie = models.ForeignKey("movies.Movie", on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="reviews")
