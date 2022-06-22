03dec6d7bb1ca7d04958d0f6a2b34275dbfbbeff
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-22 16:24:54 -0300

    fix: change the default value of updated_at field to timezone

--- users/models.py
+++ users/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .utils import CustomUserManager
 from datetime import datetime
+from django.utils import timezone
 
 class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(unique=False, null=True, max_length=255)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    updated_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=timezone.now)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
     objects = CustomUserManager()
