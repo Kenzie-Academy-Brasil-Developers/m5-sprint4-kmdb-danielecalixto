1e03b93101c7d70e38ececac5e5f813a21a5af6d
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 23:05:50 -0300

    feat: add create method in serializer not working yet

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,3 +1,4 @@
+from urllib import request
 from rest_framework import serializers
 from rest_framework.fields import CurrentUserDefault
 from .models import Review
@@ -6,22 +7,34 @@ from movies.models import Movie
 
 import ipdb
 
+class StarError(Exception):
+    pass
+
 class ReviewSerializer(serializers.ModelSerializer):
+    movie_id = serializers.SerializerMethodField()
+    critic_id = serializers.SerializerMethodField()
+
     class Meta:
         model = Review
         fields = ['stars', 'review', 'spoilers']
-        extra_kwargs = {'recommendation': {'required': False}}
-        read_only_fields = ['id', 'spoilers', 'movie_id', 'critic']
+        extra_kwargs = {'recommendation': {'required': False}, 'movie_id': {'required': False}, 'critic_id': {'required': False}}
+        read_only_fields = ['id']
 
-        def validate_stars(self, value):
-            valid_int = isinstance(value, int)
-            if valid_int==False or value<1 or value>10:
-                raise AttributeError(value, valid_int)
+    def validate_stars(self, value):
+        if value<1 or value>10:
+            raise StarError(value)
+        return value
     
-        def create(self, validated_data):
-            critic = CurrentUserDefault()
-            movie_id = self.context['request'].query_params.get('movie_id')
-            print(critic)
-            print(movie_id)
-            review = Review.objects.create(movie_id=movie_id, critic_id=critic.id, **validated_data)
-            return review
+    def get_movie_id(self, obj):
+        movie_id = self.context['request'].query_params.get('movie_id')
+        return movie_id
+
+    def get_critic_id(self, obj):
+        critic_id = self.context['request'].user
+        return critic_id.id
+
+    def create(self, validated_data):
+        validated_data['movie_id'] = self['movie_id']
+        validated_data['critic_id'] = self['critic_id']
+        review = Review.objects.create(**validated_data)
+        return review
