7c5127ad81e5a4e4094fd9d0ac8f1045b239a8d9
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-22 16:27:24 -0300

    fix: set global pagination style for PageNumberPagination

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -131,8 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     ],
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+}
