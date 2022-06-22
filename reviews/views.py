b090ca8e914013a37b26af9fff7427618cd5589a
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-21 23:05:10 -0300

    feat: add POST in Review View not working yet

--- reviews/views.py
+++ reviews/views.py
@@ -2,32 +2,30 @@ from django.shortcuts import render
 from rest_framework.views import APIView, Response, status
 
 from .models import Review
-from .serializers import ReviewSerializer
+from .serializers import ReviewSerializer, StarError
 
 from rest_framework.authentication import TokenAuthentication
 
 from rest_framework.permissions import IsAuthenticated  
 
+import ipdb
 
 class ReviewView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, _):
+    def get(self, _, movie_id):
         pass
 
-    def post(self, request):
+    def post(self, request, movie_id):
         serializer = ReviewSerializer(data=request.data)
+        
         serializer.is_valid(raise_exception=True)
         try:
             serializer.save()
-        except AttributeError as err:
-            if err.args[1]==False:
-                return Response(
-                    {"stars": "Ensure this value is an integer."},
-                    status.HTTP_400_BAD_REQUEST
-                    )
-            elif err.args[0]>10:
+        except StarError as err:
+            ipdb.set_trace()
+            if err.args[0]>10:
                 return Response(
                     {"stars": "Ensure this value is less than or equal to 10."},
                     status.HTTP_400_BAD_REQUEST
