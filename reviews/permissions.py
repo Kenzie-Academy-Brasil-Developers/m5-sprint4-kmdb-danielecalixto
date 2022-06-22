c866349199db5ebce7d55113d4711f1d1b5ea7a0
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-22 15:15:39 -0300

    feat: crete Delete custom Permission

--- reviews/permissions.py
+++ reviews/permissions.py
@@ -1,7 +1,17 @@
 from rest_framework import permissions
+from .models import Review
 
 class ListPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-      if request.method in permissions.SAFE_METHODS:
-        return True
-      return request.user.is_authenticated
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+class DeletePermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.is_superuser:
+            return True
+        return request.user.is_authenticated
+
+
+
