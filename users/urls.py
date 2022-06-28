2ecc925ea63a21072f12b252bdd678cf7345c5f9
Daniele Calixto <danielecalixtob@gmail.com>
2022-06-28 12:14:32 -0300

    refactor: add the / at the end of the urls because it's a django pattern

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-#using Django Token Authentication without JWT
 from rest_framework.authtoken import views
 
 from .views import RegisterView, UserView, LoginView, UserViewDetail
@@ -8,14 +7,6 @@ urlpatterns = [
     path("users/register/", RegisterView.as_view()),
     path("users/", UserView.as_view()),
     path("users/<int:user_id>/", UserViewDetail.as_view()),
-    path("token-auth", views.obtain_auth_token),
+    path("token-auth/", views.obtain_auth_token),
     path("users/login/", LoginView.as_view()),
 ]
-
-
-# To use jwt:
-
-# from rest_framework_simplejwt import views
-
-# path("users/login/", views.TokenObtainPairView.as_view()),
-# path("token/refresh/", views.TokenRefreshView.as_view()),
